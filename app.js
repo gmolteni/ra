ded2b82b8bb99cae34af3a508069e0835f2e3394
Guillermo Molteni <guillem2604@gmail.com>
2018-11-30 16:43:38 -0300

    obtiene ip y muestra lista

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var app = express();
+var ipArr = new Array();
+
+Number.prototype.pad = function(size) {
+  var s = String(this);
+  while (s.length < (size || 2)) {s = "0" + s;}
+  return s;
+}
+
+
 app.get('/', function (req, res) {
 
-    if ( typeof c == 'undefined' ) { c = 0; }
+    if ( typeof c == 'undefined' ) { 
+        c = 0; 
+
+    }
+        var currentdate = new Date(); 
+        var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
     c++;
-    res.send('Accedido: '+c);
+    ipArr.push(c.pad(4)+": "+datetime+": "+req.connection.remoteAddress);
+    s = '';
+    for (var indice in ipArr)
+    {
+        s += ipArr[indice] + "<br>";
+    }
+    
+    
+    res.send('Accedido: '+c+'<br><br>'+s);
     
 });
 app.listen(80, '0.0.0.0');
